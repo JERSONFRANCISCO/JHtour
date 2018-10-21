7bd7d449c35189155051453356068e8393d9ad12
JordanJL <jorjimlop@outlook.com>
2018-10-20 18:40:14 -0600

    Login

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>NOMBRE</title>
+    
+    <?php
+    require_once("global.php");
+    //<!-- Bootstrap core CSS -->
+    echo "<link href='".__APP_DSG."vendor/bootstrap/css/bootstrap.min.css' rel='stylesheet'>";
+    //<!-- Custom fonts for this template -->
+    echo "<link href='".__APP_DSG."vendor/fontawesome-free/css/all.min.css' rel='stylesheet' type='text/css'>";
+    echo "<link href='".__google_fonts."?family=Montserrat:400,700' rel='stylesheet' type='text/css'>";
+    echo "<link href='".__google_fonts."?family=Kaushan+Script' rel='stylesheet' type='text/css'>";
+    echo "<link href='".__google_fonts."?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>";
+    echo "<link href='".__google_fonts."?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>";
+    //<!-- Custom styles for this template -->
+    echo "<link href='".__CSS_PATH."login.css' rel='stylesheet'>";
+    ?>
+</head>
+    <?php
+        include_once(__VWS_PATH."login.php");
+    // <!-- Bootstrap core JavaScript -->
+    echo "<script src='".__APP_DSG."vendor/jquery/jquery.min.js'></script>";
+    echo "<script src='".__APP_DSG."vendor/bootstrap/js/bootstrap.bundle.min.js'></script>";
+    //<!-- Plugin JavaScript -->
+    echo "<script src='".__APP_DSG."vendor/jquery-easing/jquery.easing.min.js'></script>";
+    //<!-- Contact form JavaScript -->
+    echo "<script src='".__JS_PATH."jqBootstrapValidation.js'></script>";
+    echo "<script src='".__JS_PATH."contact_me.js'></script>";
+    //<!-- Custom scripts for this template -->
+    echo "<script src='".__JS_PATH."agency.js'></script>";
+    ?>
+  </body>
+</html>
